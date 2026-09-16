95975e0141541a67bb040b2b1f118010b1052ada
xmizutsuki <80939354+xmizutsuki@users.noreply.github.com>
2026-09-16 17:21:23 -0300

    fix: invalidate old PWA cache for curriculum v2

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'clinspeak-shell-v1';
+const CACHE_NAME = 'clinspeak-shell-v2';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
